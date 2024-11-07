0950b69c73911b733af5153d8b56b77f4095b301
ibraevaf <ibraevaf@akbarsdigital.ru>
2024-11-07 20:31:46 +0300

    add repositories

---
+++ DeliveryApp.Infrastructure/Adapters/Postgres/Repositories/OrderRepository.cs
@@ -0,0 +1,47 @@
+﻿using DeliveryApp.Core.Domain.Model.OrderAggregate;
+using DeliveryApp.Core.Ports;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryApp.Infrastructure.Adapters.Postgres.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public OrderRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+
+    public async Task Add(Order order)
+    {
+        if (order.Status is not null) _dbContext.Attach(order.Status);
+
+        await _dbContext.Orders.AddAsync(order);
+    }
+
+    public void Update(Order order)
+    {
+        if (order.Status is not null) _dbContext.Attach(order.Status);
+
+        _dbContext.Orders.Update(order);
+    }
+
+    public Task<Order> Get(Guid orderId) =>
+        _dbContext
+           .Orders
+           .Include(x => x.Status)
+           .SingleOrDefaultAsync(o => o.Id == orderId);
+
+    public IEnumerable<Order> GetAllCreated() =>
+        _dbContext
+           .Orders
+           .Include(x => x.Status)
+           .Where(o => o.Status == OrderStatus.Created);
+
+    public IEnumerable<Order> GetAllAssigned() =>
+        _dbContext
+           .Orders
+           .Include(x => x.Status)
+           .Where(o => o.Status == OrderStatus.Assigned);
+}
